e3fb5912bdeaae02729d1ee194b03bb2ae9a0d64
ebhemmanuel <ebh.emmanuel@gmail.com>
2014-04-09 14:24:25 -0400

    Adding files and changing the way things slide

---
+++ sf01/js/04_04.js
@@ -0,0 +1,15 @@
+$(function(){
+
+	$("img").mouseover(function(){
+
+		$(this).animate({width:"200px", opacity:1},1000);
+
+	});
+
+	$("img").mouseout(function(){
+
+		$(this).animate({width:"100px", opacity:.3},1000);
+
+	});
+
+});
