7ea21042d441fc5e246596bafa15fae2d61d5cdb
ebhemmanuel <ebh.emmanuel@gmail.com>
2014-08-13 12:39:57 -0400

    Adding the file for the creative brief.

--- public/js/effects.js
+++ public/js/effects.js
@@ -1,12 +1,30 @@
 $(document).ready(function(){
 	var		addEntry   = $("#add-entry");
 	var		addSection = $("#add-section");
-	var   moveUp     = $(".move-up");
 	var 	moveDown 	 = $(".move-down");
+	var		type       = $(".type-replace");
 	$(addSection).on("click", function(e){
 		e.preventDefault();
-		$(addEntry).toggleClass('move-down')
-		// $(addSection).text("Cancel")
-		// $(this).is($(addSection).text("+")) ? $(this).text("") : $(this).show();
+		if((type).text() === "+"){
+			$(addEntry).toggleClass("move-down");
+			$(type).text("-");
+		}
+		else if((type).text() === "-"){
+		$(addEntry).toggleClass("move-down");
+		$(type).text("+");
+		}
 	});
 });
+
+$("#signUpCta").on("click", function(){
+	$(".cta-content").animate({"margin-left":-500},4
+		00);
+	// $("#signUpContent").css("display","block")
+	// 								.animate({opacity:1},400);
+});
+
+
+
+// $(".reset").click(function() {
+//     $(this).closest('form').find("input[type=text], textarea").val("");
+// 		});
