ba3623c6ebcb82ad9ad3dd4029ef699e95f11484
ebhemmanuel <ebh.emmanuel@gmail.com>
2014-08-15 18:02:19 -0400

    adding the right files.

---
+++ backup_of_Barreto_Emmanuel_Classes.py
@@ -0,0 +1,60 @@
+#MMO Theme
+class ffxiv(object):
+	def __init__(self):
+		self.name = " "
+		self.classes = " "
+		self.level = 0
+	def dungeon(self):
+		print "Carry them time."
+	def trial(self):
+		print "I hope to god it's not Titan."
+	def pvp(self):
+		print "Get ready to get ganked."
+#Pet Theme
+class pet(object):
+	def __init__(self):
+		self.name = " "
+		self.breed = " "
+		self.age = 0
+	def angry(self):
+		print "Hiss..."
+	def loving(self):
+		print "Purr..."
+	def activity(self):
+		print "Sleeping."
+# Sports Theme
+class athelete(object):
+	def __init__(self):
+		self.sport = " "
+		self.style = " "
+		self.skill_level = " "
+	def training(self):
+		print "Have to be the best no one ever was."
+	def teamwork(self):
+		print "There's no I in team."
+	def gametime(self):
+		print "It's now or never."
+# Education Theme
+class education(object):
+	def __init__(self):
+		self.degree = " "
+		self.field = " "
+		self.cost = 0
+	def bachelor(self):
+		print "So the adventure begins."
+	def masters(self):
+		print "Two more years."
+	def doctors(self):
+		print "I really have to like it to do this."
+# health Theme
+class athelete(object):
+	def __init__(self):
+		self.food = " "
+		self.weight = " "
+		self.active = " "
+	def bodybuilding(self):
+		print "Have to get those gains."
+	def lose_weight(self):
+		print "Have to lose 15 pounds."
+	def stay_healthy(self):
+		print "Gotta eat more greens."
