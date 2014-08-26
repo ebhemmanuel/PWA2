900a8eea35ea03d8b18ec52ed2330fa837f8fb22
ebhemmanuel <ebh.emmanuel@gmail.com>
2014-08-26 02:52:11 -0400

    Adding new files.

--- public/js/effects.js
+++ public/js/effects.js
@@ -25,6 +25,196 @@ $(document).ready(function(){
 	});
 });
 
+$(function(){
+	var dashboard  = "dashboard.html"
+	function logIn(page){
+		var url  				= 'xhr/login.php';
+		var user 				= $('#user').val();
+		var pass 				= $('#pass').val();
+		var dataType 		= 'json';
+		var type  			= 'post';
+		$.ajax({
+			url:url,
+			type:type,
+			dataType:dataType,
+			data:{
+				username: user,
+				password: pass
+			},
+			success:function(response){
+				if(response.error){alert(response.error)			 }
+				else							{window.location.assign(page)}
+			}
+		});
+	};
+	function register(page){
+		var url  				= 'xhr/register.php';
+		var user 				= $('#userR').val();
+		var pass 				= $('#passR').val();
+		var email				= $('#emailR').val();
+		var dataType 		= 'json';
+		var type  			= 'post';
+		$.ajax({
+			url:url,
+			type:type,
+			dataType:dataType,
+			data:{
+				username: user,
+				password: pass,
+				email: email,
+			},
+			success:function(response){
+				if(response.error){alert(response.error)			 }
+				else							{window.location.assign(page)}
+			}
+		});
+	};
+	$("#sign-in-button").click(function(){
+		console.log("boom");
+		logIn(dashboard);
+	});
+
+		$("#sign-up-button").click(function(){
+		console.log("boom");
+		register(dashboard);
+	});
+	var log =  $('#user, #pass');
+	var sign = $('#userR, #passR');
+
+// 	function enterFn(name, page){
+// 		name.focus(function(){
+// 			name.keyup(function(e){
+// 				var code = e.which;
+// 				e.preventDefault();
+// 				if (code == 13){page};
+// 			});
+// 		});
+// 	};
+// enterFn(log, logIn(dashboard));
+// enterFn(sign, register(dashboard));
+
+log.focus(function(){
+		log.keyup(function(e, page){
+			var code = e.which;
+			e.preventDefault();
+			if (code == 13){logIn(dashboard)};
+		});
+	});
+
+	sign.focus(function(){
+		sign.keyup(function(e){
+			var code = e.which;
+			e.preventDefault();
+			if (code == 13){register(dashboard)};
+		});
+	});
+
+	$.getJSON('xhr/check_login.php', function(data){
+		$.each(data,function(key, val){
+			$('.userid').html('Welcome ' + val.first_name);
+		})
+	});
+	$('html').ready(function() {
+    $(this).closest('form').find("input[type=text], textarea").val("");
+		});
+	var logOut = $('#logOut')
+	logOut.click(function(e){
+		var url = 'xhr/logout.php';
+		e.preventDefault();
+		$.get(url, function(){
+			window.location.assign('index.html');
+		})
+	});
+
+	$('#addButton').on('click', function(){
+		var projName = $('#projectName').val(),
+		 		projDesc = $('#projectDescription').val(),
+		 		projDue  = $('#projectDueDate').val(),
+				status = $('input[name = "status"]:checked').prop('id');
+
+				$.ajax({
+					url     : 'xhr/new_project.php',
+					type    : 'post',
+					dataType: 'json',
+					data    : {
+							projectName: projName,
+							projectDescription: projDesc,
+							dueDate: projDue,
+							status: status
+					}, success: function(response) {
+						console.log('Testing for success');
+						if(response.error){
+							alert(response.error)
+						}else{
+							window.location.assign('projects.html');
+						}
+					}
+				})
+	});
+
+	var del = $('.deletebtn');
+	del.click(function(e){
+		var pid = $(this).parent().find('.projectid').val();
+		console.log('test delete');
+		$.ajax({
+			url: 'xhr/delete_project.php',
+			dataType: 'json',
+			type: 'post',
+			data: {
+				projectID: pid
+			},
+			success:function(response){
+				console.log('Testing for success');
+				if(response.error){
+					alert(response.error);
+				} else{
+					window.location.assign('projects.html');
+				}
+			}
+		});
+	});
+
+	var projects = function(){
+
+	$.ajax({
+        url: 'xhr/get_projects.php',
+        type: 'get',
+        dataType: 'json',
+        success: function(response){
+            if(response.error){
+                console.log(response.error);
+            }else{
+
+	            for(var i=0, j=response.projects.length; i < j; i++){
+	                var result = response.projects[i];
+
+	                $(".projects").append(
+	                	'<div style="border:1px solid black">' +
+						//'<div id="sortable" class="ui-state-default">' +
+	                	" <input class='projectid' type='hidden' value='" + result.id + "'>" +
+	                	" Project Name: " + result.projectName + "<br>" +
+						" Project Due Date: " + result.dueDate + "<br>" +
+	                	" Project Description: " + result.projectDescription + "<br>" +
+						" Project Status: " + result.status + "<br>"
+						+ '<button class="deletebtn">Delete</button>'
+						+ '<button class="editbtn">Edit</button>'
+						+ '</div> <br>'
+	               	);
+	            };
+						};
+					}
+				});
+		};
+			projects();
+	// (".datepicker").datepicker();
+});
+
+
+
+
+
+
+
 // $(".reset").click(function() {
 //     $(this).closest('form').find("input[type=text], textarea").val("");
 // 		});
